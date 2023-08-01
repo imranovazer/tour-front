77cf70a80c5b3277220c715a6bd716095d29d843
Azer <imranovazer@gmail.com>
2023-08-01 16:21:39 +0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Hi it is my app</div>
+    </>
+  );
+}
+
+export default App;
